e81296720a0e1532f752a6244c3cdda2fe123f9f
novmk <67250036+novmk@users.noreply.github.com>
2023-06-09 06:00:06 +0100

    Add files via upload

--- sketch_grey.js
+++ sketch_grey.js
@@ -9,5 +9,14 @@ function draw() {
   scale(-1, 1);
   image(capture, -width, 0, windowWidth, windowHeight);
   filter(GRAY);
-  tint(255, 80);  
+  tint(255, 50);  
+  let x1 = random(width);
+  let y1 = random(height);
+  let x2 = round(x1 + random(-80, 80));
+  let y2 = round(y1 + random(-80, 80));
+
+  set(x2, y2, get(x1, y1, 100, 100)); // x,y,obj
+  set(x1, y1, get(x2, y2, 200, 200)); // x,y,obj
+
+  updatePixels();
 }
