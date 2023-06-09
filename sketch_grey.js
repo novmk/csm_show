5b768784acbb0d85a84b059199366e848c395fd4
novmk <67250036+novmk@users.noreply.github.com>
2023-06-09 06:05:33 +0100

    Add files via upload

--- sketch_grey.js
+++ sketch_grey.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(7);
   capture = createCapture(VIDEO);
   capture.hide();
 }
@@ -10,13 +11,14 @@ function draw() {
   image(capture, -width, 0, windowWidth, windowHeight);
   filter(GRAY);
   tint(255, 50);  
-  let x1 = random(width);
-  let y1 = random(height);
+  let x1 = random(width*0.1, width*0.7);
+  let y1 = random(height*0.1, height*0.7);
   let x2 = round(x1 + random(-80, 80));
   let y2 = round(y1 + random(-80, 80));
 
   set(x2, y2, get(x1, y1, 100, 100)); // x,y,obj
   set(x1, y1, get(x2, y2, 200, 200)); // x,y,obj
 
+  // image(capture, x1, x2, 50 50, x2, y2, 50, 50);
   updatePixels();
 }
