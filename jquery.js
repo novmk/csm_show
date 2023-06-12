a4f73e21cdd01a39e69b308b2cbc5dd1cc259929
novmk <67250036+novmk@users.noreply.github.com>
2023-06-12 01:19:39 +0100

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
 
-	var exp01 = $("index_btn.left > a");
-	var exp01 = $("index_btn.right > a");
+	var $a01 = $(".index_btn.left > a");
+	var $a02 = $(".index_btn.right > a");
+	var $index_img = $(".index_img > img");
+	var $index_contents = $(".index_contents");
+	var $exp01 = $("#exp01");
+	var $exp02 = $("#exp02");
 
-	
+	$a01.mouseover(function() {
+		$index_img.fadeOut();
+		$index_contents.fadeOut();
+		$exp01.fadeIn();
+	});
+	$a01.mouseleave(function() {
+		$exp01.fadeOut();
+		$index_img.fadeIn();
+		$index_contents.fadeIn();
+	});
+
+	$a02.mouseover(function() {
+		$index_img.fadeOut();
+		$index_contents.fadeOut();
+		$exp02.fadeIn();
+	});
+	$a02.mouseleave(function() {
+		$exp02.fadeOut();
+		$index_img.fadeIn();
+		$index_contents.fadeIn();
+	});
+
 
 });
 
--- sketch_grey.js
+++ sketch_grey.js
@@ -10,14 +10,19 @@ function draw() {
   scale(-1, 1);
   image(capture, -width, 0, windowWidth, windowHeight);
   filter(GRAY);
-  tint(255, 60);  
+  tint(255, 50);  
   let x1 = random(width);
   let y1 = random(height);
-  let x2 = round(x1 + random(-80, 80));
-  let y2 = round(y1 + random(-80, 80));
+  let xr1 = round(x1 + random(-80, 80));
+  let yr1 = round(y1 + random(-80, 80));
+  let x2 = random(width);
+  let y2 = random(height);
+  let xr2 = round(x2 + random(-100, 100));
+  let yr2 = round(y2 + random(-100, 100));
 
-  set(x2, y2, get(x1, y1, 100, 100)); // x,y,obj
-  set(x1, y1, get(x2, y2, 200, 200)); // x,y,obj
+
+  set(xr1, yr1, get(x1, y1, 100, 100)); // x,y,obj
+  set(xr1, yr2, get(x2, y2, 200, 200)); // x,y,obj
 
   updatePixels();
 }
